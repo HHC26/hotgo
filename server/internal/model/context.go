e414968fc85d2894f9f7ee4cd36efff52b1587a7
HHC26 <44391821+HHC26@users.noreply.github.com>
2024-07-11 14:01:44 +0800

    Update context.go
    多角色

--- server/internal/model/context.go
+++ server/internal/model/context.go
@@ -25,8 +25,8 @@ type Identity struct {
 	Pid      int64       `json:"pid"             description:"上级ID"`
 	DeptId   int64       `json:"deptId"          description:"部门ID"`
 	DeptType string      `json:"deptType"        description:"部门类型"`
-	RoleId   int64       `json:"roleId"          description:"角色ID"`
-	RoleKey  string      `json:"roleKey"         description:"角色唯一标识符"`
+	RoleIds  []int64     `json:"roleIds"         description:"角色ID"`
+	RoleKeys []string    `json:"roleKeys"        description:"角色标识符"`
 	Username string      `json:"username"        description:"用户名"`
 	RealName string      `json:"realName"        description:"姓名"`
 	Avatar   string      `json:"avatar"          description:"头像"`
